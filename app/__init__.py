9a408f6e8085defab809695b5a343ad491ea3a2d
weineko <gulingxi@glxycl.top>
2023-08-07 16:53:35 +0800

    修改app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,10 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-if(os.path.exists("../config/dev_config.py")):
+if(os.path.exists("./config/dev_config.py")):
     app.config.from_pyfile('../config/dev_config.py')
     app.debug=True
 else:
     app.config.from_pyfile('../config/config.py')
 
-from app.api import upload, retrieve , auth
+from app.api import upload, retrieve, auth
