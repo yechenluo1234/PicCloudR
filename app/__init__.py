1c1ddc76d1054becae287ceca1c966324be0d6b0
weineko <gulingxi@glxycl.top>
2023-08-08 13:29:00 +0800

    调整日志输出格式

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
+import logging
 import os
 from flask import Flask
 
 app = Flask(__name__)
+
+logging.basicConfig(format='[%(asctime)s] [%(process)d] [%(levelname)s] %(message)s', level=logging.INFO)
+
 if(os.path.exists("./config/dev_config.py")):
     app.config.from_pyfile('../config/dev_config.py')
     app.debug=True
